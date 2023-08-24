8beadadfac9cb230c3056461a31d6f897a482718
Juan Lugo <juanlugodev@gmail.com>
2023-08-24 09:28:49 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// const task = document.getElementById()
+
+  const submit = document.getElementById("create-task-form");
+  const task = document.getElementById("new-task-description");
+  // const newTaskPriority = document.getElementById("new-task-priority");
+  
+function addToList() {
+  const li = document.createElement('li');
+  const button = document.createElement('button');
+  button.textContent = "x";
+  li.textContent = task.value;
+  list.append(li);
+  li.append(button);
+  
+  task.value = "";
+  // button deleting data
+  button.addEventListener("click", () => {
+    li.remove();
+  })
+
+}  
+// newTaskForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   taskList.createNewTask(newTaskDescription.value);
+//   // reseting form
+//   e.target.reset();
+// });
+
+function removeList() {
+  li.remove();
+}
+function newSubmit(submit) {
+  addToList();
+  submit.preventDefault();
+}
+
+submit.addEventListener('submit', newSubmit)
 });
